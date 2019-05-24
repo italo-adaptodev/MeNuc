e5c83eaab1dbd5072c40c26ae288664fa7f6b9ab
Italo <italo.santos@ifba.edu.br>
2019-05-24 16:54:42 -0300

    Versão com Invite funcional + BD

---
+++ app/src/main/java/prototipo/italoluis/com/fireprot3/Indicados.java
@@ -0,0 +1,26 @@
+package prototipo.italoluis.com.fireprot3;
+
+public class Indicados {
+
+    private String nomeIndicado;
+    private String emailIndicado;
+
+    public Indicados() {
+    }
+
+    public String getNomeIndicado() {
+        return nomeIndicado;
+    }
+
+    public void setNomeIndicado(String nomeIndicado) {
+        this.nomeIndicado = nomeIndicado;
+    }
+
+    public String getEmailIndicado() {
+        return emailIndicado;
+    }
+
+    public void setEmailIndicado(String emailIndicado) {
+        this.emailIndicado = emailIndicado;
+    }
+}
--- app/src/main/java/prototipo/italoluis/com/fireprot3/Invite.java
+++ app/src/main/java/prototipo/italoluis/com/fireprot3/Invite.java
@@ -1,13 +1,54 @@
 package prototipo.italoluis.com.fireprot3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Invite extends AppCompatActivity {
+
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference = database.getReference().child("Indicados");
+    Indicados indicados;
+    EditText nome, email;
+    ImageButton button_send;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite);
+        nome = findViewById(R.id.indicado1);
+        email = findViewById(R.id.indicado2);
+        button_send = findViewById(R.id.btn_send);
+        indicados = new Indicados();
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                indicados.setNomeIndicado(nome.getText().toString().trim());
+                indicados.setEmailIndicado(email.getText().toString().trim());
+
+                //reference.push().setValue(indicados);
+                reference.child(user.getUid()).push().setValue(indicados);
+                Toast.makeText(Invite.this, "ok!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
+
+
+
     }
 }
